81e8781de87e974956039f1826f8556135dfa705
JanethHdez1 <20223tn061@utez.edu.mx>
2025-12-11 21:19:21 -0600

    cambios nginx

--- client/src/modules/test/test.controller.js
+++ client/src/modules/test/test.controller.js
@@ -1,10 +1,14 @@
 const TestController = {};
 const ENV = import.meta.env;
 
-const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}/test`
+const API_URL = `${ENV.VITE_API_PROTOCOL}://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
 
 TestController.callToAPI = async () => {
-    await fetch(`${API_URL}`, {method: 'GET', headers:{"Accept":"application/JSON"  }
+    await fetch(`${API_URL}/test`, {
+        method: 'GET',
+        headers: {
+            "Accept": "application/json"
+        }
     }).then(response => response.json()).then(result => {
         console.log(result);
     }).catch(console.log);
